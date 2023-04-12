2b42d3b0992adb54a7040029302e7b0f83c40c12
Mahendra <mahendrabrowny@gmail.com>
2023-04-12 09:34:51 +0530

    override buton style functionality

--- src/app/components/ui/Button.tsx
+++ src/app/components/ui/Button.tsx
@@ -1,5 +1,7 @@
+import { cn } from "@/lib/utils";
 import { cva, VariantProps } from "class-variance-authority";
-import React, { ButtonHTMLAttributes, FC } from "react";
+import { Loader2 } from "lucide-react";
+import { ButtonHTMLAttributes, FC } from "react";
 
 const buttonVariants = cva(
   "active:scale-95 inline-flex items-center justify-center rounded-md text-sm font-medium transition-color focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none",
@@ -24,10 +26,28 @@ const buttonVariants = cva(
 
 export interface ButtonProps
   extends ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonVariants> {}
+    VariantProps<typeof buttonVariants> {
+  isLoading?: boolean;
+}
 
-const Button: FC<ButtonProps> = ({}) => {
-  return <div>Button</div>;
+const Button: FC<ButtonProps> = ({
+  className,
+  children,
+  variant,
+  isLoading,
+  size,
+  ...props
+}) => {
+  return (
+    <button
+      {...props}
+      className={cn(buttonVariants({ variant, size, className }))}
+      disabled={isLoading}
+    >
+      {isLoading ? <Loader2 className="mr-2 h-4 w-4 animate-spin" /> : null}
+      {children}
+    </button>
+  );
 };
 
 export default Button;
