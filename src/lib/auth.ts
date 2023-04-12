799f1d337d932e8bd8054357f73ab1e70ffea7da
Mahendra <mahendrabrowny@gmail.com>
2023-04-12 13:05:53 +0530

    fix import bug

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,7 @@
 import { NextAuthOptions } from "next-auth";
 import { UpstashRedisAdapter } from "@next-auth/upstash-redis-adapter";
 import { db } from "./db";
-import GoogleProvider from "next-auth/providers";
+import GoogleProvider from "next-auth/providers/google";
 
 function getGoogleCredentials() {
   const clientId = process.env.GOOGLE_CLIENT_ID;
