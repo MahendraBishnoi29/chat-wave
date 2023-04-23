bda25706fb767b1f97cea7c4a2a17f99406bd5e1
Mahendra <mahendrabrowny@gmail.com>
2023-04-23 22:40:04 +0530

    fetch friends from db

--- src/components/SidebarChatList.tsx
+++ src/components/SidebarChatList.tsx
@@ -7,9 +7,9 @@ interface SidebarChatListProps {
 const SidebarChatList: FC<SidebarChatListProps> = ({ friends }) => {
   return (
     <ul role="list" className="max-h-[25rem] overflow-y-auto -mx-2 space-y-1">
-      {/* {friends.sort.map((friend) => {
-
-     })} */}
+      {friends.sort.map((friend) => {
+        return <div key={friend.id}></div>;
+      })}
       frineds
     </ul>
   );
