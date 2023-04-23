7f88b7b96f45ef44c6ef1f9c7a6686c2a6d56186
Mahendra <mahendrabrowny@gmail.com>
2023-04-23 22:44:22 +0530

    include pathname

--- src/components/SidebarChatList.tsx
+++ src/components/SidebarChatList.tsx
@@ -1,14 +1,27 @@
-import React, { FC } from "react";
+import { usePathname, useRouter } from "next/navigation";
+import { FC, useEffect, useState } from "react";
 
 interface SidebarChatListProps {
   friends: User[];
 }
 
 const SidebarChatList: FC<SidebarChatListProps> = ({ friends }) => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const [unseenMessages, setUnseenMessages] = useState<Message[]>([]);
+
+  useEffect(() => {
+    if (pathname?.includes("chat")) {
+      setUnseenMessages((prev) => {
+        return prev.filter((msg) => !pathname.includes(msg.senderId));
+      });
+    }
+  }, [pathname]);
+
   return (
     <ul role="list" className="max-h-[25rem] overflow-y-auto -mx-2 space-y-1">
       {friends.sort.map((friend) => {
-        return <div key={friend.id}></div>;
+        return <div></div>;
       })}
       frineds
     </ul>
