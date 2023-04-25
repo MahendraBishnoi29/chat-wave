a78d8375172fbddb301c926e70787676ed2bdb7c
Mahendra <mahendrabrowny@gmail.com>
2023-04-25 14:20:38 +0530

    remove user info

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -6,7 +6,7 @@ import { authOptions } from "@/lib/auth";
 const page = async ({}) => {
   const session = await getServerSession(authOptions);
 
-  return <pre> {JSON.stringify(session)} </pre>;
+  return <pre> Dashboard </pre>;
 };
 
 export default page;
