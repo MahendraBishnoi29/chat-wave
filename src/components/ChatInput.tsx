90ce6fb89ab589f15688f71fb605839d04a36b31
Mahendra <mahendrabrowny@gmail.com>
2023-04-26 22:51:18 +0530

    fix classnames

--- src/components/ChatInput.tsx
+++ src/components/ChatInput.tsx
@@ -2,6 +2,7 @@
 
 import { FC, useRef, useState } from "react";
 import TextareaAutosizeProps from "react-textarea-autosize";
+import Button from "./ui/Button";
 
 interface ChatInputProps {
   chatPartner: User;
@@ -14,8 +15,8 @@ const ChatInput: FC<ChatInputProps> = ({ chatPartner }) => {
   const sendMessage = async () => {};
 
   return (
-    <div className="broder-t border-gray-200 px-4 pt-4 mb-2 sm:mb-0">
-      <div className="reltive flex flex-1 overflow-hidden rounded-lg shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-indigo-600">
+    <div className="border-t border-gray-200 px-4 pt-4 mb-2 sm:mb-0">
+      <div className="relative flex-1 overflow-hidden rounded-lg shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-indigo-600">
         <TextareaAutosizeProps
           ref={textAreaRef}
           onKeyDown={(e) => {
@@ -30,6 +31,24 @@ const ChatInput: FC<ChatInputProps> = ({ chatPartner }) => {
           placeholder={`Message to ${chatPartner.name}`}
           className="block w-full resize-none border-0 bg-transparent text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:py-1.5 sm:text-sm sm:leading-6"
         />
+
+        <div
+          className="py-2"
+          aria-hidden="true"
+          onClick={() => textAreaRef.current?.focus()}
+        >
+          <div className="py-px">
+            <div className="h-9" />
+          </div>
+        </div>
+
+        <div className="absolute right-0 bottom-0 flex justify-between py-2 pl-3 pr-2">
+          <div className="flex-shrink-0">
+            <Button onClick={sendMessage} type="submit">
+              Post
+            </Button>
+          </div>
+        </div>
       </div>
     </div>
   );
