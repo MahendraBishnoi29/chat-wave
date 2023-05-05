e3706bf92aa047b5e1d6c5975614fe513204c65d
Mahendra <mahendrabrowny@gmail.com>
2023-05-05 23:03:46 +0530

    pusher trigger for server side

--- src/app/api/friends/add/route.ts
+++ src/app/api/friends/add/route.ts
@@ -1,6 +1,8 @@
 import { fetchRedis } from "@/helpers/redis";
 import { authOptions } from "@/lib/auth";
 import { db } from "@/lib/db";
+import { pusherServer } from "@/lib/pusher";
+import { toPusherKey } from "@/lib/utils";
 import { addFriendValidator } from "@/lib/validations/add-friend";
 import { getServerSession } from "next-auth";
 import { z } from "zod";
@@ -10,12 +12,12 @@ export async function POST(req: Request) {
     const body = await req.json();
     const { email: emailToAdd } = addFriendValidator.parse(body.email);
 
-    const isToAdd = (await fetchRedis(
+    const idToAdd = (await fetchRedis(
       "get",
       `user:email:${emailToAdd}`
     )) as string;
 
-    if (!isToAdd) {
+    if (!idToAdd) {
       return new Response("This person does not exist", { status: 400 });
     }
 
@@ -25,14 +27,14 @@ export async function POST(req: Request) {
       return new Response("Unauthorized", { status: 401 });
     }
 
-    if (isToAdd === session.user.id) {
+    if (idToAdd === session.user.id) {
       return new Response("Why are you adding yourself 🙂", { status: 400 });
     }
 
     // check if user is already added
     const isAlreadyAdded = (await fetchRedis(
       "sismember",
-      `user:${isToAdd}:incoming_friend_requests`,
+      `user:${idToAdd}:incoming_friend_requests`,
       session.user.id
     )) as 0 | 1;
 
@@ -43,8 +45,8 @@ export async function POST(req: Request) {
     // check if user is already added
     const isAlreadyFriend = (await fetchRedis(
       "sismember",
-      `user:${isToAdd}:friends`,
-      isToAdd
+      `user:${idToAdd}:friends`,
+      idToAdd
     )) as 0 | 1;
 
     if (isAlreadyFriend) {
@@ -53,7 +55,16 @@ export async function POST(req: Request) {
       });
     }
 
-    db.sadd(`user:${isToAdd}:incoming_friend_requests`, session.user.id);
+    pusherServer.trigger(
+      toPusherKey(`user:${idToAdd}:incoming_friend_requests`),
+      "incoming_friend_requests",
+      {
+        senderId: session.user.id,
+        senderEmail: session.user.email,
+      }
+    );
+
+    db.sadd(`user:${idToAdd}:incoming_friend_requests`, session.user.id);
     return new Response("OK");
   } catch (error) {
     if (error instanceof z.ZodError) {
