018eef0bce940ce25ce1bd9b749a2a5b6f6c1279
Mahendra <mahendrabrowny@gmail.com>
2023-05-05 22:49:59 +0530

    pusher colon ragex validtor helper

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -9,3 +9,7 @@ export function chatHrefConstructor(id1: string, id2: string) {
   const sortedId = [id1, id2];
   return `${sortedId[0]}--${sortedId[1]}`;
 }
+
+export function toPusherKey(key: string) {
+  return key.replace(/:/g, "__");
+}
