8344e3625b1d8374c742afb8b41bcde42308252a
Mahendra <mahendrabrowny@gmail.com>
2023-05-13 09:59:56 +0530

    render lg and sm nav

--- app/components/sidebar/Sidebar.tsx
+++ app/components/sidebar/Sidebar.tsx
@@ -1,4 +1,5 @@
 import DesktopSidebar from "./DesktopSidebar";
+import MobileFooter from "./MobileFooter";
 
 interface SidebarProps {
   children: React.ReactNode;
@@ -8,6 +9,7 @@ export default async function Sidebar({ children }: SidebarProps) {
   return (
     <div className="h-full">
       <DesktopSidebar />
+      <MobileFooter />
       <main className="lg:pl-20 h-full">{children}</main>
     </div>
   );
