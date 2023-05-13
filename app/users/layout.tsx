910c2c94a7a249f2b36c73c298941b8f040a303c
Mahendra <mahendrabrowny@gmail.com>
2023-05-13 17:31:21 +0530

    display users in layout

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,14 +1,21 @@
+import getUsers from "../actions/getUsers";
 import Sidebar from "../components/sidebar/Sidebar";
+import UserList from "./UserList/UserList";
 
 interface UsersLayoutProps {
   children: React.ReactNode;
 }
 
 export default async function UsersLayout({ children }: UsersLayoutProps) {
+  const users = await getUsers();
+
   return (
     // @ts-expect-error Server Component
     <Sidebar>
-      <div className="h-full">{children}</div>;
+      <div className="h-full">
+        <UserList users={users} />
+        {children}
+      </div>
     </Sidebar>
   );
 }
