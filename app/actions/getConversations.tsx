d99ab38c72627d6f5e94ac4c802cbb60ea6ebe65
Mahendra <mahendrabrowny@gmail.com>
2023-05-14 18:55:36 +0530

    reusable action for fetching our conversation from DB

--- app/actions/getConversations.tsx
+++ app/actions/getConversations.tsx
@@ -1,4 +1,37 @@
-// get form db
-// order bylast messages
-// where -> userids -> has current user id or not
-// populate fields: include the user ->  then messages then populate users -> messages -> include -> sender, seen
+import prisma from "@/app/libs/prismadb";
+import getCurrentUser from "./getCurrentUser";
+
+const getConversations = async () => {
+  try {
+    const currentUser = await getCurrentUser();
+    if (!currentUser?.id) {
+      return [];
+    }
+
+    const conversations = await prisma.conversation.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+      where: {
+        userIds: {
+          has: currentUser?.id,
+        },
+      },
+      include: {
+        users: true,
+        messages: {
+          include: {
+            seen: true,
+            sender: true,
+          },
+        },
+      },
+    });
+
+    return conversations;
+  } catch (error: any) {
+    throw new Error("Failed to fetch Conversations " + error?.message);
+  }
+};
+
+export default getConversations;
