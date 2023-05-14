fa729d09330bb55550bb86233adc44f63e66e644
Mahendra <mahendrabrowny@gmail.com>
2023-05-14 18:39:59 +0530

    get conversations form DB 😃

---
+++ app/actions/getConversations.tsx
@@ -0,0 +1,4 @@
+// get form db
+// order bylast messages
+// where -> userids -> has current user id or not
+// populate fields: include the user ->  then messages then populate users -> messages -> include -> sender, seen
